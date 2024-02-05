6549d92beca985425f755bb606a50258891ad7e0
dennysihol <denny.sihol@gmail.com>
2024-02-05 11:00:22 +0700

    Feat: Dyanamic button list

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-
-  const AnswerButton ({super.key, required this.answerText, required this.onTap});
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onTap});
 
   final String answerText;
   final void Function() onTap;
@@ -12,8 +12,15 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
         foregroundColor: Colors.white,
-        backgroundColor: Color.fromARGB(255, 17, 8, 151)
+        backgroundColor: const Color.fromARGB(255, 17, 8, 151),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
       ),
       child: Text(answerText),
     );
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_quiz/answer_button.dart';
+import 'package:simple_quiz/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -13,22 +14,26 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'This is Question....',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(answerText: 'answer 1', onTap: () {} ),
-          AnswerButton(answerText: 'answer 2', onTap: () {} ),
-          AnswerButton(answerText: 'answer 3', onTap: () {} ),
-          AnswerButton(answerText: 'answer 4', onTap: () {} ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(
+              answerText: answer,
+              onTap: () {},
+            );
+          }),
         ],
       ),
     );
